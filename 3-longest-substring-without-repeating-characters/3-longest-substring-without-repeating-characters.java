1c01f76bfbeb6850d04fec123472d99a5e38b9bd
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2022-06-17 13:34:45 -0700

    Time: 14 ms (41.38%), Space: 47 MB (17.61%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,46 +1,23 @@
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         Map<Character, Integer> ans = new HashMap<>();
-//         int left =0; 
-//         int right =0;
-//         int max = 0;
-//         while(right < s.length()){
-//             if(ans.containsKey(s.charAt(right))){
-//                 ans.remove(s.charAt(left));
-//                 left++;
-//             }
-//             ans.put(s.charAt(right),right);
-//             right++;
-//             max = Math.max(max, ans.size());
-//         }
-//         return max;
-//     }
-// }
-
-class Solution
-{
-    public int lengthOfLongestSubstring(String s)
-    {
-        // O(n) time | O(n) space
-        HashSet<Character> mySet = new HashSet<>();
-        
-        int left = 0, right = 0;
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> ans = new HashMap<>();
+        int left =0; 
+        int right =0;
         int max = 0;
-        
-        while(right < s.length())
-        {            
-            if(!mySet.contains(s.charAt(right)))
-            {
-                mySet.add(s.charAt(right));
+        while(right < s.length()){
+            if(!ans.containsKey(s.charAt(right))){
+                ans.put(s.charAt(right),right);
                 right++;
-                max = Math.max(max, mySet.size());
+                max = Math.max(max, ans.size());
+                
             }
-            else
-            {
-                mySet.remove(s.charAt(left));
+            else{
+                ans.remove(s.charAt(left));
                 left++;
             }
+            
         }
         return max;
     }
 }
+
