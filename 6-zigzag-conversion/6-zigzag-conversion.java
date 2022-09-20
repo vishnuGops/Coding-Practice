25d2e80a28f5ed4a744aa18dbbff49d2aed00496
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2022-09-20 12:27:45 -0700

    Time: 17 ms (51.56%), Space: 51.6 MB (41.55%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.java
+++ 6-zigzag-conversion/6-zigzag-conversion.java
@@ -2,23 +2,26 @@ class Solution {
     public String convert(String s, int numRows) {
         StringBuilder sb = new StringBuilder();
         List<Character>[] rows = new List[numRows];
-        int k =0;
+        int k =0 ;
         int direction = (numRows == 1)? 0 : -1;
-        for(int i =0; i< numRows; ++i){
+        
+        for(int i =0; i < numRows; ++i){
             rows[i] = new ArrayList<>();
         }
         
         for(final char c : s.toCharArray()){
             rows[k].add(c);
-            if(k == 0 || k == numRows -1)
+            if(k == 0 || k == numRows - 1)
                 direction *= -1;
-            k+=direction;
+            k += direction;
         }
+        
         for(List<Character> row : rows){
             for(final char c : row){
                 sb.append(c);
             }
         }
+        
         return sb.toString();
     }
 }
