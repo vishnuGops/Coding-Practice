09e38b0cd80ae634e25df1fd376b724420ead1da
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2023-07-10 16:02:21 -0700

    Time: 0 ms (100%), Space: 45 MB (41.99%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int first = 0;
+        int len = nums.length;
+        int last = len -1;
+
+        while(first <= last){
+            int mid = first + (last - first) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if( target < nums[mid]){
+                last = mid -1;
+            }
+            else{
+                first = mid +1;
+            }
+        }
+        return -1;
+    }
+}
